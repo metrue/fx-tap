778702ced90c641c96e0250303d65cb78d25633e
metrue <h.minghe@gmail.com>
2019-12-06 09:59:45 +0800

    Brew formula update for fx version 0.8.6-alpha.34a4959

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -2,16 +2,16 @@
 class Fx < Formula
   desc "fx, a simple but powerful Function as a Service build tools"
   homepage "https://github.com/metrue/fx"
-  version "0.8.6-alpha.d7130c4"
+  version "0.8.6-alpha.34a4959"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/metrue/fx/releases/download/0.8.6-alpha.d7130c4/fx_0.8.6-alpha.d7130c4_macOS_64-bit.tar.gz"
-    sha256 "ed6375254069db9ae0d6916d5ea8af0f8d8a9277c6beb6f68e5d2fc04fe32db1"
+    url "https://github.com/metrue/fx/releases/download/0.8.6-alpha.34a4959/fx_0.8.6-alpha.34a4959_macOS_64-bit.tar.gz"
+    sha256 "6e535dec3c53e8144ff720814e1b23dccd3a9bf23650369bcb26b72499d7729c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/metrue/fx/releases/download/0.8.6-alpha.d7130c4/fx_0.8.6-alpha.d7130c4_Tux_64-bit.tar.gz"
-      sha256 "41dc8403bbe2d1fe8e605e8bace760b3e0e16264aae125c9059a108fa9543df7"
+      url "https://github.com/metrue/fx/releases/download/0.8.6-alpha.34a4959/fx_0.8.6-alpha.34a4959_Tux_64-bit.tar.gz"
+      sha256 "840e1dd0a26ff9f0c35dc2bd6cc3b833046b17f0be19c5de2151d7d69eb62494"
     end
   end
 
